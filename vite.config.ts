22ec07953873963f200d81cceb32a2013770c612
sainivas456 <nivas.gandham456@gmail.com>
2025-04-01 18:55:24 -0400

    final production failures

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,15 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { defineConfig } from "vite";
 
-export default defineConfig(({ }) => ({
-  base: '/My_PortFolio2/',
+export default defineConfig({
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-  ].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
