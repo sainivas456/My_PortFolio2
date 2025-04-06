c0830e73f98a049120a7592af111ded0793646f5
sainivas456 <nivas.gandham456@gmail.com>
2025-04-06 11:39:10 -0400

    vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
+import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
@@ -19,6 +20,7 @@ const App = () => (
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
+        <Analytics /> {/* <-- Add this inside BrowserRouter but outside Routes */}
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
